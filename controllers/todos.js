038e416d832411fae788518346a6554abb55d07b
Denis <dbatrakov1@gmail.com>
2022-08-30 22:05:54 -0500

    added name of user who leav the idea in todo.ejs

--- controllers/todos.js
+++ controllers/todos.js
@@ -5,15 +5,16 @@ module.exports = {
         console.log(req.user)
         try{
             const todoItems = await Todo.find({userId:req.user.id})
+            const userName = await Todo.find({userId:req.user.id})
             const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            res.render('todos.ejs', {name: userName, todos: todoItems, left: itemsLeft, user: req.user})
         }catch(err){
             console.log(err)
         }
     },
     createTodo: async (req, res)=>{
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+            await Todo.create({name: req.body.userName, todo: req.body.todoItem, completed: false, userId: req.user.id})
             console.log('Todo has been added!')
             res.redirect('/todos')
         }catch(err){
